01c8d6ecc4ba0cc609dcda8898cb2d8aebc5f52f
renJava <rendg68@gmail.com>
2023-02-24 22:07:46 +0300

    a 0.1.0 - Готово на массивах. Самый короткий и быстрый

--- src/academy/kata/scalc/SCalc.java
+++ src/academy/kata/scalc/SCalc.java
@@ -9,9 +9,6 @@ import static java.lang.System.out;
 //    "$Yj@rgЁ1Ыi" - "^ej%rgЁ1"   //             //    "" - "^ej%rgЁ1"     //
 //    "Yej!~Ё 2Ыы" - "ej!~Ё 2"    //
 //    "&(jrg&)" *   10            //             //    ""    *   9         //
-//         "<Ёёe2^!`~V" /   10    //             //         "<Ёёe2^+-Vы" / 10      //
-//      "47rgЫЁё" /      2        //             //      "00" /      2           //
-
 public class SCalc {
     static final String INPUT_ERROR = "!!!Некорректный ввод!!!";
     public static void main(String[] args) {
@@ -25,11 +22,11 @@ public class SCalc {
                                                     
                     Вводите строчные операнды (не более 10 символов каждый). Начинайте ввод операнда строго с двойных
                     кавычек. Количество пробелов до, после, между, внутри операндов и операторов не лимитируется.
-                    1-й операнд - всегда строчный - любая последовательность символов, c пробелами, но только без
-                    3-х управляющих символов см. в скобках: ("+*) и не более 10 символов между кавычками.
-                    Например: "e47#@&r/-~"
+                    1-й операнд - всегда строчный - любая последовательность символов, c пробелами, кроме управляющего
+                    символа см. в скобках: (") и не более 10 символов между кавычками.
+                    Например: "e47#@+*/-~"
                     2-й операнд, как первый, но только при сложении и вычитании (+,-) и также обернут в кавычки.
-                    Например: "$Yj@rgЁ1Ыi". Причем 2-й операнд должен быть отделен от оператора хотя бы одним пробелом.
+                    Например: "$Yj@rgЁ1Ыi".
 
                     При умножении и делении (*,/) второй операнд - натуральное число <=10 - БЕЗ КАВЫЧЕК!!!.
                                         
@@ -47,78 +44,54 @@ public class SCalc {
         while (validateIn.equals(INPUT_ERROR));
     }
 
-    //    Задаем все переменные метода.
-    static String isValidate(String expression) {
-
-//                                      Сложение и вычитание
+    static String isValidate(String expression) {          //Проверка корректности ввода
 
 //     Регулярное выражение для + и - 1 : *\"[a-zA-Z_0-9а-яА-ЯёЁ]{0,10}\" *[+,-] *\"[a-zA-Z_0-9а-яА-ЯёЁ]{0,10}\" *
 //                                      :^ *\"[^\"-+/*]{0,10}\" *[+,-] *\"[^\"-+/*]{0,10}" *
+        String[] cutEx = fullTrim(expression);
+        String operator = cutEx[3];
 
         if (expression.matches(
-                "^ *\"[^\"+]{0,10}\" *[+,-] *\"[^\"+]{0,10}\" *")) {
+                "^ *\"[^\"]{0,10}\" *[+,-] *\"[^\"]{0,10}\" *")) {
 
-//      Принимаем из метода выражение без боковых пробелов и кавычек
-            String fullTrimS = fullTrim(expression);
-            String[] operand = extract(fullTrimS, '+');
-            int operator = Integer.parseInt(operand[0]);
-
-            return (operator > 0) ?
-                    Pm.sAdd(operand[1], operand[2]) :
-                    Pm.sSubtract(operand[1], operand[2]);
+            return operator.equals("+") ? Pm.sAdd(cutEx[1], cutEx[2]) : Pm.sSubtract(cutEx[1], cutEx[2]);
         }
-
-//                                      Умножение и деление
-
-//     Регулярное выражение для * и / : ^ *\"[a-zA-Z_0-9а-яА-ЯёЁ]{0,10}\"\s*[*,\/]\s*(?:[1-9]|10) *$
-//                                    : ^ *\"[^\"]{0,10}\" *[*,/] *(?:[1-9]|10) *$
-
         if (expression.matches(
-                "^ *\"[^\"*]{0,10}\" *[*,/] +(?:[1-9]|10) *$")) {
+                "^ *\"[^\"]{0,10}\" *[*,/] *(?:[1-9]|10) *$")) {
 
-            String fullTrimS = fullTrim(expression);
-            String[] operand = extract(fullTrimS, '*');
-            int operator = Integer.parseInt(operand[0]);
-
-            return (operator > 0) ?
-                    Md.sMultiply(operand[1], operand[2]) :
-                    Md.sDivide(operand[1], operand[2]);
+            return operator.equals("*") ? Md.sMultiply(cutEx[1], cutEx[2]) : Md.sDivide(cutEx[1], cutEx[2]);
         }
         return INPUT_ERROR;
+
     }
-
-//        Метод возвращает контрольные символы в массив: внутренние кавычки (1!2), оператор и длинну выражения
-    static String[] extract (String workingExpression, char controlChar) {
-//        String cutToQuote = fullTrim(workingExpression); //Принимаем тело выражения
-        int lengthT = workingExpression.length();
-        String[] operand = new String[3];
-        int quotePos0 = workingExpression.indexOf('\"');
-        int operator = workingExpression.indexOf(controlChar, quotePos0);
-        operand[0] = String.valueOf(operator);
-        int quotePos1 = workingExpression.substring(0, lengthT - 1).lastIndexOf('\"');
-        operand[1] = workingExpression.substring(0, quotePos0);
-
-        if (controlChar == '+') {operand[2] = workingExpression.substring(quotePos1 + 1, lengthT - 1);}
-        else {operand[2] = workingExpression.substring(lengthT - 2, lengthT).trim();}
-
-        return operand;
-    }
-
-//    Вырезает и возвращает тело выражения
-    static String fullTrim (String workingExpression) {
-      String trimExpression = workingExpression.trim().substring(1); //Отбрасываем боковые пробелы начальную "
-      int lengthCt = trimExpression.length();
-      return trimExpression.substring(0, lengthCt);
+    //    Вырезать и вернуть операнды и оператор
+    private static String[] fullTrim(String workExpression) {
+        String[] trimEx = new String[4];
+        trimEx[0] = workExpression.trim().substring(1);             //Отбрасываем боковые пробелы и первую (")
+        int lengthT = trimEx[0].length();
+        int quote1 = trimEx[0].indexOf("\"");
+        trimEx[1] = trimEx[0].substring(0, quote1);                                      //Первый операнд
+        int quoteLast = trimEx[0].lastIndexOf("\"");
+        int quote2 = trimEx[0].indexOf("\"", quote1+1);
+        if (quote1 != quoteLast) {
+            trimEx[2] = trimEx[0].substring(quote2+1, lengthT-1);                        //Второй операнд
+            trimEx[3] = trimEx[0].substring(quote1+1, quote2-1).trim();                  //Оператор
+        } else {
+            String operatorAndInt = trimEx[0].substring(quote1+1, lengthT).trim();       //Поле оператора с числом/
+            trimEx[3] = String.valueOf(operatorAndInt.charAt(0));                        //Чистый оператор * или /
+            trimEx[2] = operatorAndInt.substring(1).trim();                     //Числовой оператор
+        }
+        return trimEx;
     }
 
     //            В 2-х классах по 2 метода
     //          Блок сложения и вычитания
-    private static class Pm {                                  //Сложение - конкатенация
+    private static class Pm {                                 //Сложение - конкатенация
         static String sAdd(String a, String b) {
             return a + b;
         }
 
-        static String sSubtract(String a, String b) {               //Вычитание
+        static String sSubtract(String a, String b) {         //Вычитание
 //      При Вычетании - вырезаем найденное слово из строки или возвращаем уменьшаемое обратно
             int substringBegin = a.indexOf(b);
             return (substringBegin > -1) ? a.substring(0, substringBegin) +
@@ -127,15 +100,16 @@ public class SCalc {
     }
     //          Блок умножения и деления
     private static class Md {
-        static String sMultiply(String a, String b) {          //Умножение
+        static String sMultiply(String a, String b) {         //Умножение
 //      При Умножении - повторяем заданное слово b раз и обрезаем результат после 40-го символа
             String sMultiple = a.repeat(Integer.parseInt(b));
             return (sMultiple.length() <= 40) ? sMultiple : sMultiple.substring(0, 40) + "...";
         }
 
-        static String sDivide(String a, String b) {          //Деление
+        static String sDivide(String a, String b) {           //Деление
             int d = Integer.parseInt(b);
             return (a.length() >= d) ? a.substring(0, a.length() / d) : "Делитель больше делимого";
         }
+
     }
 }
