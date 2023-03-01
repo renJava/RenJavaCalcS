c33b9201cded024d97f33a8f96cc39b7b163c39a
renJava <rendg68@gmail.com>
2023-03-01 23:07:44 +0300

    v-0.7.0 Осталось разобраться с Exception и перобразования вывода в латинский

--- src/academy/kata/calc_test1/CalcLatinArabic.java
+++ src/academy/kata/calc_test1/CalcLatinArabic.java
@@ -2,13 +2,12 @@ package academy.kata.calc_test1;
 
 import java.util.Scanner;
 
-import static java.lang.System.in;
-import static java.lang.System.out;
+import static java.lang.System.*;
 
 public class CalcLatinArabic {
     static final String INPUT_ERROR = "!!!Некорректный ввод!!!";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String expression;
         String validateIn;
         do {
@@ -34,28 +33,29 @@ public class CalcLatinArabic {
         while (validateIn.equals(INPUT_ERROR));
     }
 
-    static String isValidate(String expression) {          //Проверка корректности ввода
+    static String isValidate(String expression) throws Exception {          //Проверка корректности ввода
 
-        final String beginer = "^";
-        final String reOperandAr = "[ \t]*(?:[1-9]|10)[ \t]*"; //Цифра [1-10] вокруг пробелы и Tabs
-        final String reOperandL = "[ \t]*[IVX]{1,4}[ \t]*"; //Любой набор до 10-ти сим. в "", в пробелах и Tab
-        final String reOperators = "[-+*/]";
+      final String beginer = "^";
+      final String reOperandAr = "[ \t]*(?:[1-9]|10)[ \t]*"; //Цифра [1-10] вокруг пробелы и Tabs
+      final String reOperandL = "[ \t]*[IVX]{1,4}[ \t]*"; //Любой набор до 10-ти сим. в "", в пробелах и Tab
+      final String reOperators = "[-+*/]";
 
-        final String regexCompositeAr = beginer + reOperandAr + reOperators + reOperandAr;     //Регулярка +- в "" <= 10 симв.
-        final String regexCompositeL = reOperandL + reOperators + reOperandL;   //Регулярка */ в "" <= 10 симв.
+      final String regexCompositeAr = beginer + reOperandAr + reOperators + reOperandAr;//Регулярка +- в "" <= 10 симв.
+      final String regexCompositeL = beginer + reOperandL + reOperators + reOperandL;   //Регулярка */ в "" <= 10 симв.
 
 
-        if (expression.matches(regexCompositeAr)) {
-            boolean isLatin = false;
-            return fullTrim(expression, isLatin);
-        }
-//            else if (expression.matches(regexCompositeL)) {
-//        }
-        return INPUT_ERROR;
+      if (expression.matches(regexCompositeAr)) {
+          return fullTrim(expression, false);
+      }
+          else if (expression.matches(regexCompositeL)) {
+          return fullTrim(expression, true);
+
+      }
+      return INPUT_ERROR;
     }
 
     //    Порезать выражение и вернуть результат
-    private static String fullTrim(String workExpression, boolean ifLatin) {
+    private static String fullTrim(String workExpression, boolean ifLatin) throws Exception {
         String[] trimEx = new String[3];
         String trimS = workExpression.trim();                         //Отбрасываем боковые пробелы и Tabs
         int lengthT = trimS.length();
@@ -65,12 +65,25 @@ public class CalcLatinArabic {
         trimEx[0] = operator;
         trimEx[1] = trimS.substring(0, operatorPos).trim();           //1-й строковый операнд
         trimEx[2] = trimS.substring(operatorPos+1, lengthT).trim();   //2-й строковый операнд
-        return calculator(trimEx);                                 //(!ifLatin)? calculator(trimEx) : toArabic(trimEx);
+        return (!ifLatin)? calculator(trimEx) : calculator(toArabic(trimEx));
     }
 
-//    private static String[] toArabic(String[] trimEx) {
-//    return calculator();
-//    }
+    private static String[] toArabic(String[] convert) throws Exception {
+        String[] hasConverted = new String[3];
+        hasConverted[0] = convert[0];
+        for (int i = 1; i <= 2; i++) {
+            try {
+                LatinEnum.valueOf(convert[i]);
+            } catch (IllegalArgumentException e) {
+                out.println("Числа нет в латинском: " + convert[i] + " !!!");
+            }
+            hasConverted[i] = String.valueOf(LatinEnum.valueOf(convert[i]).ordinal() + 1);
+            if (Integer.parseInt(hasConverted[i]) > 10) {
+                throw new Exception("Вводимые операнды должны быть <=10");
+            }
+        }
+        return hasConverted;
+    }
 
     private static String detectOperator(String isOperator) {
         if (isOperator.contains("+")) return "+";
