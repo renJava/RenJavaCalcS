70a58866be5595c8253b6521bcbe1dbf3f39d5a1
renJava <rendg68@gmail.com>
2023-03-14 11:12:16 +0300

    Задача 1.4.10 - не проходит кодстайл

--- src/academy/kata/educational_process/IsWeekend_1_4_10.java
+++ src/academy/kata/educational_process/IsWeekend_1_4_10.java
@@ -3,31 +3,41 @@ package academy.kata.educational_process;
 import java.util.Scanner;
 
 /**
- *Реализовать метод, который будет принимать строку с названием дня недели, и отвечать, является ли этот день выходным.
- *  Список дней недели: "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday".
- *  Если строка не соответствует ни одному дню недели отправлять false.
+ * Реализовать метод, который будет принимать строку с названием дня недели, и отвечать, является ли этот день выходным.
+ * Список дней недели: "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday".
+ * Если строка не соответствует ни одному дню недели отправлять false.
  */
 public class IsWeekend_1_4_10 {
     public static boolean isWeekend(String weekday) {
         //Твой код здесь
-        String [] dayOfWeek = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-        System.out.print("\nВведите номер дня недели: ");
-        Scanner scanner = new Scanner(System.in);
-        String dayNumber = scanner.nextLine();
-        System.out.println("Сегодня выходной? " + isWeekend(dayNumber));
+        String[] daysOfWeek = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+                "Sunday"};
 
-        return switch (dayOfWeek[Integer.parseInt(weekday)]){
-            case "Monday" -> false;
-            case "Tuesday" -> false;
-            case "Wednesday" -> false;
-            case "Thursday" -> false;
-            case "Friday" -> false;
-            case "Saturday" -> true;
-            case "Sunday" -> true;
-            default -> false;
-        };
+        String dayOfWeek = "";
+
+        for (String arr : daysOfWeek) {
+
+            if (arr.equals(weekday)) {
+                dayOfWeek = weekday;
+                break;
+            }
+        }
+
+        switch (dayOfWeek) {
+
+            case "Saturday", "Sunday": {
+                return true;
+            }
+            default: {
+                return false;
+            }
+        }
     }
 
     public static void main(String[] args) {
+        System.out.print("\nВведите день недели по-английски с большой буквы: ");
+        Scanner scanner = new Scanner(System.in);
+        String dayNumber = scanner.nextLine();
+        System.out.println(dayNumber + " выходной? " + isWeekend(dayNumber));
     }
 }
